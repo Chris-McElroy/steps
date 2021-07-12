7127d48e42a95489b40869bb6e31853a09128ac9
ChristopherMcElroy <csmcelroy@me.com>
2021-07-12 15:57:52 -0700

    Initial Commit

---
+++ steps/stepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepsApp.swift
+//  steps
+//
+//  Created by Chris McElroy on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct stepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
